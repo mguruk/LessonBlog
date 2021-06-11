5df9365775b80b8c97285732ab456033dd23fb9c
mguruk <kumar540manish@gmail.com>
2021-06-12 00:07:01 +0530

    initialize project

---
+++ src/main/java/com/microguru/lessonblog/LessonBlogApplication.java
@@ -0,0 +1,13 @@
+package com.microguru.lessonblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microguru/lessonblog/LessonBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microguru.lessonblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
