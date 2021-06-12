01530d8995955f9b24144ffe50996f09d1cc6e6c
mguruk <kumar540manish@gmail.com>
2021-06-12 20:50:48 +0530

    index page and controller closes #6

---
+++ src/main/java/com/microguru/lessonblog/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package com.microguru.lessonblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author manish
+ *
+ */
+
+
+@Controller
+public class IndexController {
+	
+	
+	
+	@RequestMapping({"","/","index","index.html"})
+	public String index() {
+		return "index";
+		
+	}
+
+}
